f30aae647355a25316080b680cf8c12eddcf7504
sidiq02 <emailsidiq02@gmail.com>
2026-06-25 18:44:24 +0700

    last commit

--- app/src/main/java/id/ac/polbeng/sidik/appbarexample/MenuFragment.kt
+++ app/src/main/java/id/ac/polbeng/sidik/appbarexample/MenuFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
 import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.sidik.appbarexample.MenuFragment
 
 class MenuFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.fragment_menu)
+        setContentView(R.layout.activity_menu_fragment)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
